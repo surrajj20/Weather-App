84370138e34b5017fe335f33996e1420e93cbc9d
surrajj20 <shah.suraj1200@gmail.com>
2020-07-16 23:13:49 +0530

    location_screen added

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
 const kButtonTextStyle = TextStyle(
   fontSize: 20,
   fontFamily: 'Spartan MB',
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60.0
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
 );
